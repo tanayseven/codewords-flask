20e8ecde7ce8df2864828b70c3d51570cd12a528
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-03-25 00:31:11 +0530

    Addded user list page

--- flask_app/account.py
+++ flask_app/account.py
@@ -1,2 +1,18 @@
 class Account(object):
-    pass
+
+    def __init__(self, user_name='', full_name='', balance=0):
+        self.__user_name = user_name
+        self.__full_name = full_name
+        self.__balance = balance
+
+    @property
+    def user_name(self):
+        return self.__user_name
+
+    @property
+    def full_name(self):
+        return self.__full_name
+
+    @property
+    def balance(self):
+        return self.__balance
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 
+import sys
+sys.path.append('./')
+from flask_app.account import Account
 app = Flask(__name__)
 
 
@@ -29,6 +32,11 @@ def sign_up():
     if request.method == 'POST':
         print("Signed up user: " + request.form['username'])
         return render_template('home_page.jinja')
+
+
+@app.route('/users_list', methods=['GET'])
+def users_list():
+    return render_template('users_list.jinja', user_list=[Account(user_name='tanay'), Account(user_name='tushar')])
 
 
 if __name__ == "__main__":
