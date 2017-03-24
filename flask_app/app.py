d63844b5da4d8b97176994fcf0c32c9ed2bafb8e
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-03-24 19:40:30 +0530

    Added a home page

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home_page.jinja')
 
 
 @app.route("/hello")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
