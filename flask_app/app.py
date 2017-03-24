c250396c9e44c5e237a3959b24e64aeadb1f8657
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-03-25 00:08:37 +0530

    Added users to sign up and to login, not yet storing them

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,24 @@ def home():
 @app.route("/hello")
 def hello():
     return "Hello World!"
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.jinja')
+    if request.method == 'POST':
+        print("Logged in user: " + request.form['username'])
+        return render_template('home_page.jinja')
+
+
+@app.route('/sign_up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'GET':
+        return render_template('sign_up.jinja')
+    if request.method == 'POST':
+        print("Signed up user: " + request.form['username'])
+        return render_template('home_page.jinja')
 
 
 if __name__ == "__main__":
